c43896ee7227c62b5a38612955a2b5aefa9ca2dd
Christian F. Brinkmann <christian.brinkmann09@gmail.com>
2024-09-12 12:32:31 +0200

    update

--- js/translate.js
+++ js/translate.js
@@ -1,3 +1,10 @@
+
+function getUrlParameter(name) {
+    const regex = new RegExp(`[?&]${name}=([^&#]*)`);
+    const results = regex.exec(window.location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
 // Popup translations for supported languages
 const popupTranslations = {
     en: {
